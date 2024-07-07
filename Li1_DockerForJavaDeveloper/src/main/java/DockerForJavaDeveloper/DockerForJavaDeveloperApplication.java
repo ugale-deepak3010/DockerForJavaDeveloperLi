9ee3dbcc83cc1a910dfa59bed3e60e751a869ba0
Deepak Ugale <ugale.deepak3010@gmail.com>
2024-07-07 14:59:37 +0530

    I created folder structure.

---
+++ Li1_DockerForJavaDeveloper/src/main/java/DockerForJavaDeveloper/DockerForJavaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package DockerForJavaDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerForJavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerForJavaDeveloperApplication.class, args);
+	}
+
+}
---
+++ Li1_DockerForJavaDeveloper/src/test/java/DockerForJavaDeveloper/DockerForJavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package DockerForJavaDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerForJavaDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
